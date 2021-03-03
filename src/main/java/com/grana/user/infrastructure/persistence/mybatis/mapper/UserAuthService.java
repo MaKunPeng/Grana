662e954338511067c3e5cd3f8582ca9a1a1eee70
kunpeng <makunpeng1231@qq.com>
2021-03-03 20:10:39 +0800

    xxx

--- src/main/java/com/grana/user/infrastructure/persistence/mybatis/mapper/UserAuthService.java
+++ src/main/java/com/grana/user/infrastructure/persistence/mybatis/mapper/UserAuthService.java
@@ -2,12 +2,14 @@ package com.grana.user.infrastructure.persistence.mybatis.mapper;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.grana.user.domain.model.user.UserAuth;
+import org.springframework.stereotype.Service;
 
 /**
  * UserAuth Pesistence Service
  * @author Aaron Ma
  * @Date 2021-3-3 15:9:32
  */
+@Service
 public class UserAuthService extends ServiceImpl<UserAuthMapper, UserAuth> {
 
 }
